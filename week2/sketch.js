83353b9b8db49c02d856edd8f656edda113e05d0
turbshott <turbshott@gmail.com>
2026-09-09 19:16:59 -0400

    Work before break
    Week 2 coursework before break

--- week2/sketch.js
+++ week2/sketch.js
@@ -1,10 +1,38 @@
-
-
+let x
+let ySpeed
+let xSpeed
+let diameter = 50
+let y = diameter+1
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  
+  createCanvas(windowWidth, windowHeight)
+  x = random(width)
+  ySpeed = random(-10, 10)
+  xSpeed = random(-10, 10)
+  rectMode(CENTER)
+  strokeWeight(5)
 }
 
 function draw() {
+  background(25, 80)
+  stroke(300,200,0)
+  fill(40)
+  ellipse(x, y, diameter, diameter)
   
+  if(y >= height - diameter/2){
+    ySpeed = -ySpeed
+  }
+  if(y <= diameter/2){
+    ySpeed = -ySpeed
+  }
+  if(x >= width - diameter/2){
+    xSpeed = -xSpeed
+  }
+  if(x <= diameter/2){
+    xSpeed = -xSpeed
+  }
+
+  y+=ySpeed
+  x+=xSpeed
+
+  print(y)
 }
