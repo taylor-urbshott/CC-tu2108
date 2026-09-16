d7c9a90cc6d57e9baab5fc0d0e9c92af41f6e2dc
turbshott <turbshott@gmail.com>
2026-09-16 16:47:02 -0400

    Added popping squares to sketch 4

--- assignment2/sketch4/sketch.js
+++ assignment2/sketch4/sketch.js
@@ -1,6 +1,6 @@
 let count = 5;
 let minSize = 20, maxSize = 200;
-let sizeList = [], xList = [], yList = [];
+let sizeList = [], xList = [], yList = [], hoverTime = [0, 0, 0, 0, 0]; 
 let bounce = 10;
 
 
@@ -28,18 +28,28 @@ function setup() {
 
 function draw() {
   background(0);
-  for (let i = 0; i < count; i++) {
+  for (let i = 0; i < sizeList.length; i++) {
     let size = sizeList[i];
     if ((yList[i] - size/2) < mouseY && mouseY < (yList[i] + size/2) && (xList[i] - size/2) < mouseX && mouseX < (xList[i] + size/2)) {
+      hoverTime[i] += deltaTime;
       strokeWeight(2);
       xList[i] = random(xList[i] - bounce, xList[i] + bounce);
-      yList[i] = random(yList[i] - bounce, yList[i] + bounce);
-      rect(xList[i], yList[i], sizeList[i]);
-      strokeWeight(1);
-    } else {
-      rect(xList[i], yList[i], sizeList[i]);
+      yList[i] = random(yList[i] - bounce, yList[i] + bounce); 
+    } else if (hoverTime[i] > 0){
+      hoverTime[i] -= deltaTime;
+    } 
+    fill(hoverTime[i]/8, 0, 0)
+    rect(xList[i], yList[i], sizeList[i]);
+    strokeWeight(1);
+    if (hoverTime[i] > 2000) {
+      rect(xList[i], yList[i], sizeList[i] * 2);
+      hoverTime.splice(i, 1);
+      xList.splice(i, 1);
+      yList.splice(i, 1);
+      sizeList.splice(i, 1);
     }
   }
 }
+
 
 
